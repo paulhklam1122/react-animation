8563116d9b01894513be9447af358be6541e13d8
Paul Lam <paulhklam1122@gmail.com>
2019-02-16 13:14:29 -0800

    meetup finish

---
+++ src/app/Root.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
+import posed, { PoseGroup } from 'react-pose'
+import Element from './containers/Element'
+
+import Home from './containers/Home'
+import Product from './containers/Product'
+
+const AnimatedRoute = posed.div({
+  enter: { opacity: 1, delay: 100, beforeChildren: true },
+  exit: { opacity: 0 }
+})
+
+class Root extends Component {
+  state = {
+    activeMenu: 'Home'
+  }
+  handleItemClick = (activeMenu, history, path) => {
+    this.setState({ activeMenu })
+    history.push(path)
+  }
+  render() {
+    const { activeMenu } = this.state
+    return (
+      <Router>
+        <Route
+          render={({ location, history }) => {
+            return (
+              <div>
+                <Menu inverted>
+                  <Menu.Item
+                    name='Home'
+                    active={activeMenu === 'Home'}
+                    onClick={() => this.handleItemClick('Home', history, '/')}
+                  />
+                  <Menu.Item
+                    name='Product'
+                    active={activeMenu === 'Product'}
+                    onClick={() =>
+                      this.handleItemClick('Product', history, '/product')
+                    }
+                  />
+                  <Menu.Item
+                    name='Elment'
+                    active={activeMenu === 'Element'}
+                    onClick={() =>
+                      this.handleItemClick('Element', history, '/element')
+                    }
+                  />
+                </Menu>
+                <PoseGroup>
+                  <AnimatedRoute key={Math.random()}>
+                    <Switch>
+                      <Route path='/' exact component={Home} />
+                      <Route path='/product' component={Product} />
+                      <Route path='/element' component={Element} />
+                    </Switch>
+                  </AnimatedRoute>
+                </PoseGroup>
+              </div>
+            )
+          }}
+        />
+      </Router>
+    )
+  }
+}
+
+export default Root
---
+++ src/app/containers/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Fade from 'react-reveal/Fade'
+import Flip from 'react-reveal/Flip'
+import Roll from 'react-reveal/Roll'
+class Home extends Component {
+    render(){
+      return (
+        <div>
+        <Fade left>
+          <div style={{fontSize: 50, margin: 40}}>Hello I'm Home</div>
+        </Fade>
+        <Flip left>
+          <div style={{fontSize: 50, margin: 40}}>Hello I'm Home</div>
+        </Flip>
+        <Roll left>
+          <div style={{fontSize: 50, margin: 40}}>Hello I'm Home</div>
+        </Roll>
+      </div>
+    )
+  }
+}
+
+export default Home
